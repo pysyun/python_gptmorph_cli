082e2d4b6a56e81372603648be27fdb04044a69b
Syun Lee <pysyun@vitche.com>
2023-10-17 00:55:33 +0300

    - configured the project as a CLI tool.

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
     name="mrph",
-    version="1.0.1",
+    version="1.0.2",
     scripts=["bin/mrph"],
-    py_modules=["flows.morph"]
+    py_modules=["flows.morph"],
+    install_requires=['openai', 'python-dotenv', 'git+https://github.com/pysyun/pysyun_conversation_flow.git']
 )

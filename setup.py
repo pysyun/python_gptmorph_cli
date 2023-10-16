12ffd0e1f4f15e52f4a3dfcebe151779446b48fd
Syun Lee <pysyun@vitche.com>
2023-10-17 01:11:13 +0300

    - configured the project as a CLI tool.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mrph",
-    version="1.0.5",
+    version="1.0.6",
     scripts=["bin/mrph"],
     py_modules=["flows.morph"],
     install_requires=['openai', 'python-dotenv',

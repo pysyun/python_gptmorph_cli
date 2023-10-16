d8be8d430b347c9ac85ae65711c8f3243570cb76
Syun Lee <pysyun@vitche.com>
2023-10-17 00:13:40 +0300

    - writing generated content to a file.

--- flows/morph.py
+++ flows/morph.py
@@ -112,6 +112,11 @@ Once the OpenAI API key is added, you can proceed with running the program.
             response = MorphBot.augment_chat(messages)
 
             file_name = action["context"].get("generate_file_name")
+
+            # Save the response to a text file
+            with open(file_name, 'w', encoding='utf-8') as file:
+                file.write(response)
+
             text = f"mrph> Your \"{file_name}\" file was saved."
             await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"], text=text)
             await nested_transition(action)

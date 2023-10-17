ec8ff695b02bf810491c9642cc051f4e2f9b7e5f
Syun Lee <pysyun@vitche.com>
2023-10-17 22:53:39 +0300

    - capability to patch existing code.

--- flows/morph.py
+++ flows/morph.py
@@ -92,12 +92,39 @@ Once the OpenAI API key is added, you can proceed with running the program.
         return transition
 
     @staticmethod
+    def build_patch_transition():
+
+        openai_api_key = os.getenv("OPENAI_API_KEY")
+
+        async def transition(action):
+
+            if not openai_api_key:
+                text = "mrph> Please, configure the LLM API key as stated in \"/settings\"."
+            else:
+                text = "mrph> Enter the file name to be patched:"
+
+            await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"], text=text)
+
+        return transition
+
+    @staticmethod
     def build_generate_file_name_input_transition():
 
         async def transition(action):
             file_name = action['text']
             action["context"].add("generate_file_name", file_name)
             text = f"mrph> Ok, I will create a file \"{file_name}\" when finished. What should be in this file?"
+            await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"], text=text)
+
+        return transition
+
+    @staticmethod
+    def build_patch_file_name_input_transition():
+
+        async def transition(action):
+            file_name = action['text']
+            action["context"].add("patch_file_name", file_name)
+            text = f"mrph> Loaded \"{file_name}\". How to augment that?"
             await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"], text=text)
 
         return transition
@@ -133,6 +160,50 @@ Once the OpenAI API key is added, you can proceed with running the program.
         return transition
 
     @staticmethod
+    def build_patch_prompt_input_transition(nested_transition):
+        async def transition(action):
+
+            file_name = action['context'].get("patch_file_name")
+
+            try:
+                # Load file contents
+                with open(file_name, 'r', encoding='utf-8') as file:
+                    file_contents = file.read()
+
+                prompt = action['text']
+
+                messages = [{
+                    "role": "system",
+                    "content": "Let's re-factor the file provided."
+                }, {
+                    "role": "assistant",
+                    "content": f"Original file:\n\n{file_contents}"
+                }, {
+                    "role": "user",
+                    "content": prompt
+                }]
+
+                # Augment the file contents based on the user's prompt
+                response = MorphBot.augment_chat(messages)
+
+                # Parse code blocks
+                code_blocks = re.findall(r"```(.*?)\n(.*?)\n```", response, re.DOTALL)
+
+                # Save code blocks to a text file
+                for language, code_block in code_blocks:
+                    with open(file_name, 'w', encoding='utf-8') as file:
+                        file.write(f"{code_block}\n")
+
+                text = f"mrph> File \"{file_name}\" has been augmented based on your prompt."
+                await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"], text=text)
+                await nested_transition(action)
+            except Exception as e:
+                text = f"mrph> An error occurred while processing the file: {str(e)}"
+                await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"], text=text)
+
+        return transition
+
+    @staticmethod
     def build_exit_transition():
 
         async def transition(action):
@@ -145,9 +216,10 @@ Once the OpenAI API key is added, you can proceed with running the program.
             '''mrph> Welcome to the GPT Morph CLI Bot! You are currently in the main menu.
             
 Please choose one of the following options:
-1. /generate - Generate a new file for your project.
-2. /settings - Display your LLM settings.
-3. /graph - Graphviz representation for this bot's API.
+1. /generate - Generate a new file for your project based on a human language description.
+2. /patch - Update an existing file (re-factor) by prompting what the update should be.
+3. /settings - Display your LLM settings.
+4. /graph - Graphviz representation for this bot's API.
 
 To execute a command, type the corresponding option and press Enter.
 
@@ -176,9 +248,28 @@ You can always return to the main menu by typing "/start".''',
                 on_transition=self.build_generate_file_name_input_transition()) \
             .edge("/generate_prompt_input", "/start", "/start") \
             .edge("/generate_prompt_input", "/start", "/exit", on_transition=self.build_exit_transition()) \
+            .edge("/generate_prompt_input", "/start", "/exit", on_transition=self.build_exit_transition()) \
             .edge(
                 "/generate_prompt_input",
                 "/start",
                 None,
                 matcher=re.compile("^.*$"),
-                on_transition=self.build_generate_prompt_input_transition(main_menu_transition))
+                on_transition=self.build_generate_prompt_input_transition(main_menu_transition)) \
+            .edge("/start", "/patch_file_name_input", "/patch", on_transition=self.build_patch_transition()) \
+            .edge("/patch_file_name_input", "/start", "/start") \
+            .edge("/patch_file_name_input", "/start", "/exit", on_transition=self.build_exit_transition()) \
+            .edge("/patch_file_name_input", "/start", "/settings", on_transition=self.build_settings_transition()) \
+            .edge(
+                "/patch_file_name_input",
+                "/patch_prompt_input",
+                None,
+                matcher=re.compile("^.*$"),
+                on_transition=self.build_patch_file_name_input_transition()) \
+            .edge("/patch_prompt_input", "/start", "/start") \
+            .edge("/patch_prompt_input", "/start", "/exit", on_transition=self.build_exit_transition()) \
+            .edge(
+                "/patch_prompt_input",
+                "/start",
+                None,
+                matcher=re.compile("^.*$"),
+                on_transition=self.build_patch_prompt_input_transition(main_menu_transition))

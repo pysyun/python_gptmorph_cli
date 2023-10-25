408dbe243758ecae99ed4dc5c52a81c9dee921ee
Syun Lee <pysyun@vitche.com>
2023-10-25 03:40:24 +0300

    - the crypto audit flow.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="mrph",
-    version="1.0.12",
+    version="1.0.13",
     scripts=["bin/mrph"],
-    py_modules=["flows.morph"],
+    py_modules=["flows.morph", "settings"],
     install_requires=['openai', 'python-dotenv',
                       'pysyun_conversation_flow@git+https://github.com/pysyun/pysyun_conversation_flow.git',
                       'python_claude_web_authenticator@git+https://github.com/pysyun/python_claude_web_authenticator'

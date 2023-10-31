5ba03480421fc0ae49f773d0736bf26a25564eb8
Syun Lee <pysyun@vitche.com>
2023-11-01 01:35:40 +0200

    - added the LLM Dialog class supporting basic arithmetic operations on dialogs.

---
+++ llm_dialog.py
@@ -0,0 +1,29 @@
+import datetime
+import json
+
+
+class LLMDialog:
+
+    def __init__(self, speaker_field="role", text_field="content"):
+        self.speaker_field = speaker_field
+        self.text_field = text_field
+        self.conversation = []
+
+    def assign(self, speaker, text, time=None):
+        if time is None:
+            time = datetime.datetime.now()
+        self.conversation.append({
+            self.speaker_field: speaker,
+            self.text_field: text,
+            "time": time
+        })
+
+    def json(self):
+        return json.dumps(self.conversation)
+
+    def __add__(self, other):
+        combined = LLMDialog(speaker_field=self.speaker_field,
+                             text_field=self.text_field)
+
+        combined.conversation = self.conversation + other.conversation
+        return combined

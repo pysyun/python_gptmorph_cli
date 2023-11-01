18640a639e25f09df252e27e2c9250b6a4278b91
Syun Lee <pysyun@vitche.com>
2023-11-01 02:38:52 +0200

    - taking only source code files for the folder context.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="mrph",
-    version="1.0.17",
+    version="1.0.18",
     scripts=["bin/mrph"],
-    py_modules=["flows.morph", "settings"],
+    py_modules=["flows.morph", "settings", "llm_dialog", "folder_context_dialog"],
     install_requires=['openai', 'python-dotenv',
                       'pysyun_conversation_flow@git+https://github.com/pysyun/pysyun_conversation_flow.git',
                       'python_claude_web_authenticator@git+https://github.com/pysyun/python_claude_web_authenticator'

4141518ab9d4db814fd5c70f044fd5dd2aded551
pysyun <48061047+pysyun@users.noreply.github.com>
2024-02-08 18:57:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mrph",
-    version="1.0.34",
+    version="1.0.35",
     scripts=["bin/mrph"],
     py_modules=["flows.morph", "settings", "llm_dialog", "context_folder_dialog"],
     install_requires=['openai', 'python-dotenv',

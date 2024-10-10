e6c81110ed978979a195a48edc5f1fdac832a062
Syun Lee <pysyun@vitche.com>
2024-10-10 22:22:40 +0300

    )

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="mrph",
-    version="1.0.50",
+    version="1.0.51",
     scripts=["bin/mrph"],
-    py_modules=["flows.morph", "settings", "llm_dialog", "context_folder_dialog", "ollama_processor"],
+    py_modules=["flows.morph", "settings", "llm_dialog", "context_folder_dialog", "ollama_processor", "openai_client"],
     install_requires=['openai', 'python-dotenv', 'ollama', 'setuptools',
                       'pysyun_conversation_flow@git+https://github.com/pysyun/pysyun_conversation_flow.git',
                       'python_claude_web_authenticator@git+https://github.com/pysyun/python_claude_web_authenticator'

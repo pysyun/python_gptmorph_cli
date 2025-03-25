8182437845883ae01da929e8e85301d414bd68ca
pysyun <48061047+pysyun@users.noreply.github.com>
2025-03-25 20:32:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mrph",
-    version="1.0.52",
+    version="1.0.53",
     scripts=["bin/mrph"],
     py_modules=["flows.morph", "settings", "llm_dialog", "context_folder_dialog", "ollama_processor", "openai_client"],
     install_requires=['openai', 'python-dotenv', 'ollama', 'setuptools',
